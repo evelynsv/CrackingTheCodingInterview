09685dd48d120b78d34c9295b396a265512428d7
Evelyn Vieira <36212959+evelynsv@users.noreply.github.com>
2024-02-20 09:06:11 +0000

    Is Unique solutions and tests

--- src/main/java/cracking/the/code/interview/chapter1/Question1_1.java
+++ src/main/java/cracking/the/code/interview/chapter1/Question1_1.java
@@ -5,23 +5,43 @@ import java.util.Set;
 
 public class Question1_1 {
 
-    public static Boolean isUnique(String word) {
-        int charDiffValue = 0;
-        char initialChar = ' ';
+    // Solution without using additional data structure
+    // Time Complexity O(n^2)
+    public static boolean isUnique(String word) {
+    int numberOfEqualChars = 0;
 
-        for (int i = 0; i <= word.length() - 1; i++) {
-            int diff = Character.compare(initialChar, word.charAt(i));
-            charDiffValue = charDiffValue + diff;
-            initialChar = word.charAt(i);
+        for (int i = 0; i < word.length(); i++) {
+            char charToCompare = word.charAt(i);
+            for (int j = 0; j < word.length(); j++) {
+                int diff = Character.compare(charToCompare, word.charAt(j));
+                if (diff == 0) {
+                    numberOfEqualChars++;
+                }
+            }
         }
-
-        if (charDiffValue > 0) {
-            return true;
-        } else {
+        if ((numberOfEqualChars - word.length()) > 0) {
             return false;
+        } else {
+            return true;
         }
     }
+        
+        //----------Book solution to understand later-------------
 
+        // int checker = 0;
+        
+        // for (int i = 0; i < word.length(); i++) {
+        //     int val = word.charAt(i) - 'a';
+        //     if ((checker & (1 << val)) > 0) {
+        //         return false;
+        //     }
+        //     checker |= (1 << val);
+        // }
+        // return true;
+    
+
+    // Solution using Set data structure
+    // Time Complexity = O(n)
     static Boolean isUniqueSet(String word) {
         Set<Character> charSet = new HashSet<Character>();
     
--- src/test/java/cracking/the/code/interview/chapter1/Question1_1Test.java
+++ src/test/java/cracking/the/code/interview/chapter1/Question1_1Test.java
@@ -8,13 +8,28 @@ public class Question1_1Test {
     @Test
     public void test() {
 
+        Assertions.assertEquals(true, Question1_1.isUnique("abc"));
+        Assertions.assertEquals(true, Question1_1.isUnique("dehjkpor"));
         Assertions.assertEquals(false, Question1_1.isUnique("aaa"));
-        Assertions.assertEquals(false, Question1_1.isUnique("abc"));
         Assertions.assertEquals(false, Question1_1.isUnique("aba"));
+        Assertions.assertEquals(false, Question1_1.isUnique("abba"));
+        Assertions.assertEquals(false, Question1_1.isUnique("abac"));
+        Assertions.assertEquals(false, Question1_1.isUnique("abb"));
+        Assertions.assertEquals(false, Question1_1.isUnique("abca"));
+        Assertions.assertEquals(false, Question1_1.isUnique("aabbcc"));
+        Assertions.assertEquals(false, Question1_1.isUnique("aaabbb"));
+        Assertions.assertEquals(false, Question1_1.isUnique("abbcdef"));
 
         Assertions.assertEquals(true, Question1_1.isUniqueSet("abc"));
         Assertions.assertEquals(false, Question1_1.isUniqueSet("aaa"));
         Assertions.assertEquals(false, Question1_1.isUniqueSet("aba"));
+        Assertions.assertEquals(false, Question1_1.isUniqueSet("abba"));
+        Assertions.assertEquals(false, Question1_1.isUniqueSet("abac"));
+        Assertions.assertEquals(false, Question1_1.isUniqueSet("abb"));
+        Assertions.assertEquals(false, Question1_1.isUniqueSet("abca"));
+        Assertions.assertEquals(false, Question1_1.isUniqueSet("aabbcc"));
+        Assertions.assertEquals(false, Question1_1.isUniqueSet("aaabbb"));
+        Assertions.assertEquals(false, Question1_1.isUniqueSet("abbcdef"));
     }
 
 }
